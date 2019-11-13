882bef0d82e052b553a7d3bf3ee78b2c85c711df
kitadesega <segarian8@gmail.com>
2019-11-13 17:24:53 +0900

    viewファイル変更

--- resources/views/layouts/admin_login.blade.php
+++ resources/views/layouts/admin_login.blade.php
@@ -75,29 +75,14 @@
                 </div>
             </div>
         </nav>
-        <!-- 4列をサイドメニューに割り当て -->
-        <div class="container-fluid">
-            <div class="row">
-        <div class="col-sm-3 px-1 bg-dark min-vh-100">
-            <div class="py-2 sticky-top flex-grow-1">
-                <div class="nav flex-sm-column">
-                    <a href="" class="nav-link d-none d-sm-inline">Sidebar</a>
-                    <a href="" class="nav-link">Link</a>
-                    <a href="" class="nav-link">Link</a>
-                    <a href="" class="nav-link">Link</a>
-                    <a href="" class="nav-link">Link</a>
-                    <a href="" class="nav-link">Link</a>
-                </div>
-            </div>
-        </div>
+
 
         <div class="col" id="main">
 
             @yield('content')
 
         </div>
-    </div>
-        </div>
+
     </div>
 </body>
 </html>
