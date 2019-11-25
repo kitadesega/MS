0d8b2b6166e1b8fcb24fae1c3753623d22a32efe
kitadesega <segarian8@gmail.com>
2019-11-25 23:27:27 +0900

    view少し変更

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,53 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-{{--<div class="container">--}}
-{{--    <div class="row justify-content-center">--}}
-{{--        <div class="col-md-12">--}}
-{{--            <div class="card">--}}
-{{--                <div class="card-header">全本</div>--}}
-
-{{--                <div class="card-body">--}}
-
-{{--                    <div class="row">--}}
-{{--                        @foreach($books as $book)--}}
-{{--                        <div class="col-sm-6 col-md-4">--}}
-{{--                            <a href="/book/show/{{ $book->id }}" class="card">--}}
-{{--                                <img class="card-img" src="{{ asset('image/'.$book->image) }}" alt="...">--}}
-{{--                            </a>--}}
-{{--                            <div class="card-body">--}}
-{{--                                <h5 class="card-title">{{$book->title}}</h5>--}}
-{{--                                --}}{{--   <p class="card-text"style="height: 100px;overflow: hidden; ">{{$book->detail}}</p>--}}
-{{--                                @if($book->rental_flag)--}}
-{{--                                    --}}{{--  貸出美:{{$book->startDay}}--}}
-{{--                                    <a href="#" class="btn btn-danger">返却日:{{$book->returnDay}}</a>--}}
-{{--                                @else--}}
-{{--                                    <form action="/rental/rentConfirm" method="post">--}}
-{{--                                        @csrf--}}
-{{--                                        <input type="hidden" name="bookId" value="{{$book->id}}">--}}
-{{--                                        <button type="submit" class="btn btn-primary">レンタル</button>--}}
-{{--                                    </form>--}}
-{{--                                @endif--}}
-{{--                            </div>--}}
-{{--                            @if($book->starAvg)--}}
-{{--                            <div class="star-ratings-sprite" style="display: inline-block">--}}
-{{--                                <span style="width:{{ $book->starAvg }}%" class="star-ratings-sprite-rating"></span>--}}
-{{--                            </div>--}}
-{{--                                <span>({{ $book->reviewCount }})件</span>--}}
-
-{{--                            @endif--}}
-{{--                        </div>--}}
-{{--                        @endforeach--}}
-
-{{--                    </div>--}}
-{{--                    <div class="d-flex justify-content-center">--}}
-{{--                        {{ $books->links() }}--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</div>--}}
 <div class="container">
 
     <h4 style="border-bottom: double 5px #FFC778;">貸出履歴からのおすすめ</h4>
@@ -103,6 +56,7 @@
                 </a>
             </div>
         @endforeach
+
 
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/common.css') }}">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -82,70 +83,136 @@
     </style>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    トップ
-                </a>
-                <a class="navbar-brand" href="/search/index">
-                    検索
-                </a>
-                <a class="navbar-brand" href="/rental/rentBookInput">
-                    レンタル
-                </a>
-                <a class="navbar-brand" href="/rental/returnBookInput">
-                    返却
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+{{--    <div id="app">--}}
+{{--        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">--}}
+{{--            <div class="container">--}}
+{{--                <a class="navbar-brand" href="{{ url('/') }}">--}}
+{{--                    トップ--}}
+{{--                </a>--}}
+{{--                <a class="navbar-brand" href="/search/index">--}}
+{{--                    検索--}}
+{{--                </a>--}}
+{{--                <a class="navbar-brand" href="/rental/rentBookInput">--}}
+{{--                    レンタル--}}
+{{--                </a>--}}
+{{--                <a class="navbar-brand" href="/rental/returnBookInput">--}}
+{{--                    返却--}}
+{{--                </a>--}}
+{{--                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">--}}
+{{--                    <span class="navbar-toggler-icon"></span>--}}
+{{--                </button>--}}
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+{{--                <div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
+{{--                    <!-- Left Side Of Navbar -->--}}
+{{--                    <ul class="navbar-nav mr-auto">--}}
 
-                    </ul>
+{{--                    </ul>--}}
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                @if (Route::has('register'))
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                @endif
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+{{--                    <!-- Right Side Of Navbar -->--}}
+{{--                    <ul class="navbar-nav ml-auto">--}}
+{{--                        <!-- Authentication Links -->--}}
+{{--                        @guest--}}
+{{--                            <li class="nav-item">--}}
+{{--                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>--}}
+{{--                            </li>--}}
+{{--                            <li class="nav-item">--}}
+{{--                                @if (Route::has('register'))--}}
+{{--                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>--}}
+{{--                                @endif--}}
+{{--                            </li>--}}
+{{--                        @else--}}
+{{--                            <li class="nav-item dropdown">--}}
+{{--                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>--}}
+{{--                                    {{ Auth::user()->name }} <span class="caret"></span>--}}
+{{--                                </a>--}}
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+{{--                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">--}}
+{{--                                    <a class="dropdown-item" href="{{ route('logout') }}"--}}
+{{--                                       onclick="event.preventDefault();--}}
+{{--                                                     document.getElementById('logout-form').submit();">--}}
+{{--                                        {{ __('Logout') }}--}}
+{{--                                    </a>--}}
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+{{--                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">--}}
+{{--                                        @csrf--}}
+{{--                                    </form>--}}
+{{--                                </div>--}}
+{{--                            </li>--}}
+{{--                        @endguest--}}
+{{--                    </ul>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </nav>--}}
+
+{{--        <main class="py-4">--}}
+{{--            @yield('content')--}}
+{{--        </main>--}}
+{{--    </div>--}}
+
+    <header class="d-flex justify-content-end">
+        <div class="daikei">
+            <a href="/"><p>ホーム<br>(ホームへ戻る)</p></a>
+            <div class="daikei-bg"></div>
+        </div>
+        <div class="d-flex gengo">
+            <div>一般</div>
+            <div>かな</div>
+            <div>English</div>
+            <div>
+                <a class="btn btn-primary" href="#modal-01" role="button">ログイン</a>
+            </div>
+        </div>
+    </header>
+
+    <div class="row">
+        <aside class="col-2">
+            <div class="side">
+                <p>マイページ</p>
+                <div class="side-bg"></div>
+            </div>
+            <div class="side">
+                <p>お知らせ</p>
+                <div class="side-bg"></div>
+            </div>
+            <div class="side">
+                <p>貸出ランキング</p>
+                <div class="side-bg"></div>
+            </div>
+            <div class="side">
+                <p>利用登録申請</p>
+                <div class="side-bg"></div>
+            </div>
+        </aside>
+        <div class="col-10 p-3">
+            @yield('content')
+            </div>
+
+
+
+            <!--  ログインモーダル  -->
+            <div class="modal-wrapper" id="modal-01">
+                <a href="#!" class="modal-overlay"></a>
+                <div class="modal-window">
+                    <div class="modal-content">
+                        <div class="space">入力欄</div>
+                        <a class="btn btn-primary" href="" role="button">ログイン</a>
+                    </div>
+                    <a href="#!" class="modal-close">×</a>
                 </div>
             </div>
-        </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+
+
+        </div>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="./js/bootstrap.min.js"></script>
+
+    <script type="text/javascript">
+        $('.bs-component [data-toggle="popover"]').popover();
+        $('.bs-component [data-toggle="tooltip"]').tooltip();
+    </script>
 </body>
 </html>
